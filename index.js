713ed625b16bc5e8db7397497d13fc0aa9270b5f
susanirago22 <susaniragoorani@gmail.com>
2022-11-24 08:08:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+
+
+var cats = ["Milo","Otis","Garfield"];
+
+
+function destructivelyAppendCat()
+{
+    cats.push("Ralph")
+}
+function destructivelyPrependCat()
+
+{
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat()
+{
+    cats.pop("Garfield")
+}
+function destructivelyRemoveFirstCat()
+{
+    cats.shift();
+}
+function appendCat(name)
+{
+    return [...cats, "Broom"];
+
+}
+function prependCat(name)
+{
+    return ["Arnold", ...cats]
+}
+function removeLastCat()
+{
+        return cats.slice(0,2)
+      
+}
+function removeFirstCat()
+{
+    return cats.slice(1);
+}
